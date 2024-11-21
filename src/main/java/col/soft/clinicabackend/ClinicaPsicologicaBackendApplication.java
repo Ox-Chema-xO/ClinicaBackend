02ac5bc5c1121b5b3f731cc051da18a996563d58
Chema <manuelchema999@gmail.com>
2024-11-21 10:13:32 -0500

    add corsConfigurer

--- src/main/java/col/soft/clinicabackend/ClinicaPsicologicaBackendApplication.java
+++ src/main/java/col/soft/clinicabackend/ClinicaPsicologicaBackendApplication.java
@@ -2,6 +2,9 @@ package col.soft.clinicabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ClinicaPsicologicaBackendApplication {
@@ -9,5 +12,15 @@ public class ClinicaPsicologicaBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaPsicologicaBackendApplication.class, args);
 	}
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+                }
+            };
+        }
+
 
 }
